e1b9cd3aee80eb2e146643d5b05f34a52782e86d
gitpark1 <ppjs1009@naver.com>
2022-09-28 00:02:11 +0900

    class_variable

--- python_study/circle.py
+++ python_study/circle.py
@@ -1,8 +1,10 @@
 import math
 
 class Circle:
+    count=0
     def __init__(self,rad=1.0):
         self.__rad=rad      #private
+        Circle.count+=1
     
     def setRad(self,r):     #setter
         self.__rad=r    
@@ -14,15 +16,16 @@ class Circle:
         return math.pi*self.__rad*self.__rad
 
     def __str__(self):
-        return 'rad= {}, Area= {}'.format(self.__rad,math.pi*self.__rad*self.__rad)
+        return 'rad= {}, Area= {}'.format(self.__rad,self.calcArea())
 
     def __eq__(self,other):
         return self.__rad==other.__rad
     
 
-
+print(Circle.count)
 c=Circle(10)
 b=Circle(9)
+print(Circle.count)
 print(c.getRad())
 print(c.calcArea())
 print(c)
