71af9bf033d59f1b52f0652d1ca5415b0871f606
gitpark1 <101036622+gitpark1@users.noreply.github.com>
2022-09-29 15:47:47 +0900

    Update 55_dictionary_use.py

--- programmers1/55_dictionary_use.py
+++ programmers1/55_dictionary_use.py
@@ -17,3 +17,27 @@ def solution(survey, choices):
     
     return ans
 solution(["AN", "CF", "MJ", "RT", "NA"],[5, 3, 2, 7, 5])
+
+"""
+def solution(survey, choices):
+    answer = ''
+    RTCFJMAN = [0,0,0,0,0,0,0,0]
+    str = "RTCFJMAN"
+    for i in range(len(survey)):
+        RTCFJMAN[str.index(survey[i][1])] += choices[i]-4           
+        - 순수하게 두가지 성격중 많이 나타내는 성격을 표현하기 때문에, 비동의와 동의의 크기비교를 통해서 구할수 있다.
+        - 이 코드를 다시 해석해볼것
+
+
+    if(RTCFJMAN[0]>=RTCFJMAN[1]): answer+= "R"
+    else: answer+="T"
+    if(RTCFJMAN[2]>=RTCFJMAN[3]): answer+= "C"
+    else: answer+="F"
+    if(RTCFJMAN[4]>=RTCFJMAN[5]): answer+= "J"
+    else: answer+="M"
+    if(RTCFJMAN[6]>=RTCFJMAN[7]): answer+= "A"
+    else: answer+="N"
+
+
+    return answer
+    """
