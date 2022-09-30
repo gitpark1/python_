d698a5b0805542d9ba653c17b7dab93ed7c4cb62
gitpark1 <ppjs1009@naver.com>
2022-09-30 22:10:16 +0900

    singleton, monostate pattern

---
+++ python_study/monostate.py
@@ -0,0 +1,20 @@
+class Monostate:
+    __shared_state={'a':13}
+    __test=5
+    def __init__(self):
+        self.__dict__=self.__shared_state
+    #본래 __dict__는 객체의 변수에 대한 값을 딕셔너리 형태로 보유하는 속성
+    #위의 문장으로 인해서 같은 딕셔너리를 공유하게 되었다.
+
+
+b1=Monostate()
+b2=Monostate()
+
+b1.a=44
+b1.b=33
+print(b1.test)
+print('object b1:',b1)
+print('object b2:',b2)
+
+print('b1 state',b1.__dict__)
+print('b2 state',b2.__dict__)
---
+++ python_study/singleton.py
@@ -0,0 +1,15 @@
+class Singleton:
+    def __init__(cls):
+        if not hasattr(cls,'instance'):  #instance라는 속성이 없다면
+            print('make instance')
+            cls.instance=super().__new__(cls)   #super -> object?
+        return cls.instance
+
+#Singleton클래스의 객체는 생성될 때, if문 hasattr을 검사하게 되고, instance 속성이 존재, 객체가 존재한다면,
+#그 객체를 리턴한다(1개를 넘어서 추가적인 객체가 생성되지 않는다.)
+
+s1=Singleton()
+print("object created",s1)
+
+s2=Singleton()
+print("object created",s2)
