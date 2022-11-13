5b6c3591dff83e13738c2529dec2ee226b7fec17
gitpark1 <ppjs1009@naver.com>
2022-11-14 00:21:03 +0900

    11.14

--- python_study/class.py
+++ python_study/class.py
@@ -1,5 +1,14 @@
 class test:
-    def test2():
-        print("ADFA")
+    def __init__(self,a=10,b=10):
+        self.a=a
+        self.b=b
+    def test2(self):
+        print(self.__dict__)
+        self.__dict__={'A':12,'B':20}
 
-test.test2()
+c=test()
+c.test2()
+c.test2()
+c.a=30
+c.test2()
+c.test2()
--- python_study/static_method.py
+++ python_study/static_method.py
@@ -3,10 +3,22 @@ class Calc:
     def __init__(self,a):
         self.num=a
         Calc.count+=1
-    
+    """
+    def test2(self,c):      생성자 외부에서도 선언가능, 관용적으로 생성자에서 모두 처리한다
+        self.c=c
+        print(c)
+    """
     @classmethod        #classmethod의 경우 cls 사용(self와 유사한 변수) cls로 클래스 속성에 접근
     def print_count(cls):
-        print('{}번 생성. num = {}'.format(cls.count))
+        cls.a=5
+        print('{}번 생성'.format(cls.count))
+    
+    @classmethod
+    def clstest(cls):
+        cls.a=7
+        print("clsmethod test",cls.a)
+        cls.print_count()
+        print("clsmethod test",cls.a)
 
     @staticmethod       #staic이라서 self사용 X
     def add(a,b):
@@ -16,8 +28,9 @@ class Calc:
     def mul(a,b):
         print(a*b)
 
-Calc.add(10,20)
+##Calc.add(10,20)
 print(Calc.count)
 a=Calc(10)
 print(Calc.count)
 Calc.print_count()
+Calc.clstest()
